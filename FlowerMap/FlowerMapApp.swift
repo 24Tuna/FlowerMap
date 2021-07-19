3137b50017ae0a65888c5d3336cd0e85aa40d8a8
24Tuna <21cm0124@jec.ac.jp>
2021-07-19 18:03:59 +0900

    プロジェクト作成

---
+++ FlowerMap/FlowerMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerMapApp.swift
+//  FlowerMap
+//
+//  Created by cmStudent on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct FlowerMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
