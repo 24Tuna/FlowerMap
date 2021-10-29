ebe92c9cc570038b18a64e4acc3dd050acf031d9
24Tuna <21cm0124@jec.ac.jp>
2021-10-29 12:05:00 +0900

    追加

--- FlowerMap/PostAlertView.swift
+++ FlowerMap/PostAlertView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct PostAlertView: View {
+    @Binding var showingAlert : Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            GeometryReader{ geometry in
+                EmptyView()
+            }
+            .background(Color.gray.opacity(0.6))
+            .opacity(self.showingAlert ? 1.0 : 0.0)
+            .animation(.easeIn(duration: 0.25))
+            .onTapGesture {
+                self.showingAlert = false
+            }
+            
+            VStack{
+                Text("投稿しますか？")
+                
+                HStack{
+                    //キャンセルボタン
+                    Button(action: {
+                        showingAlert = false
+                    }){
+                        Text("キャンセル")
+                    }
+                    
+                    //投稿ボタン
+                    Button(action: {
+                        //投稿する処理を書く
+                        showingAlert = false
+                    }){
+                        Text("投稿")
+                            .foregroundColor(Color("appBlue"))
+                    }
+                    
+                }//HStack
+            }//VStack
+            .padding()
+            .background(Color("buttonFontColor"))
+        }
     }
 }
 
 struct PostAlertView_Previews: PreviewProvider {
     static var previews: some View {
-        PostAlertView()
+        PostAlertView(showingAlert: Binding.constant(true))
     }
 }
