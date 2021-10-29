cd9380077303da61d5207d1ce5454b37fa391781
honoka-22 <86650944+honoka-22@users.noreply.github.com>
2021-10-29 10:07:03 +0900

    タグ検索時の一覧表示

--- FlowerMap/View/Tag/TagListView.swift
+++ FlowerMap/View/Tag/TagListView.swift
@@ -5,32 +5,32 @@
 //  Created by cmStudent on 2021/10/29.
 //
 
-import SwiftUI
-
-struct TagListView: View {
-    @ObservedObject var viewModel: TagViewModel
-    @Binding var searchTagText: String  // 検索テキスト
-    @Binding var tagsArray: [String]    // タグの配列
-    @Binding var isTagList: Bool        // TagListを開く
-    
-    var tags: [Tag] {
-        return searchTagText.isEmpty ? viewModel.tags : viewModel.filteredTags(searchTagText)
-    }
-    
-    var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(tags) { tag in
-                    TagCell(tag: tag)
-                        .padding(.leading)
-                        .onTapGesture {
-                            //押したときの処理
-                            tagsArray.append(tag.name)  // 配列にタグを追加する
-                            searchTagText = ""          // テキストフィールドをリセットする
-                            isTagList = false           // リストを閉じる
-                        }
-                }
-            }
-        }
-    }
-}
+//import SwiftUI
+//
+//struct TagListView: View {
+//    @ObservedObject var viewModel: TagViewModel
+//    @Binding var searchTagText: String  // 検索テキスト
+//    @Binding var tagsArray: [String]    // タグの配列
+//    @Binding var isTagList: Bool        // TagListを開く
+//    
+//    var tags: [Tag] {
+//        return searchTagText.isEmpty ? viewModel.tags : viewModel.filteredTags(searchTagText)
+//    }
+//    
+//    var body: some View {
+//        ScrollView {
+//            LazyVStack {
+//                ForEach(tags) { tag in
+//                    TagCell(tag: tag)
+//                        .padding(.leading)
+//                        .onTapGesture {
+//                            //押したときの処理
+//                            tagsArray.append(tag.name)  // 配列にタグを追加する
+//                            searchTagText = ""          // テキストフィールドをリセットする
+//                            isTagList = false           // リストを閉じる
+//                        }
+//                }
+//            }
+//        }
+//    }
+//}
