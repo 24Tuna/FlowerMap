9799baf47bbb05c6b3e447492a6b478c1430ef9d
miyasaka35 <21cm0135@jec.ac.jp>
2021-10-29 10:48:35 +0900

    ログイン＆新規登録画面(仮)

---
+++ FlowerMap/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FlowerMap
+//
+//  Created by cmStudent on 2021/10/29.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
