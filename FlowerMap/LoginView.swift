4351271129dce4d7a1647168dbc7bc2889ba3b40
miyasaka35 <21cm0135@jec.ac.jp>
2021-10-29 11:58:59 +0900

    ログイン＆新規登録画面

--- FlowerMap/LoginView.swift
+++ FlowerMap/LoginView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var inputEmail: String = ""
+    @State var inputPassword: String = ""
+    @State private var showCreateAccount = false
+    
+    let fieldSize = UIScreen.main.bounds.width / 1.5
+    let logoWidth = UIScreen.main.bounds.width / 1.5
+    let logoHeight = UIScreen.main.bounds.width / 1.5
+    let loginBtnWidth = UIScreen.main.bounds.width / 2
+    let loginBtnHeight = UIScreen.main.bounds.height / 13
+    let passWidth = UIScreen.main.bounds.width / 1.5
+    let passHeight = UIScreen.main.bounds.height / 20
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 25) {
+                Image("ohanavi_logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: logoWidth, height: logoHeight)
+                
+                VStack(spacing: 25)  {
+                    TextField("メールアドレス", text: $inputEmail)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .frame(width: fieldSize)
+                    
+                    
+                    SecureField("パスワード", text: $inputPassword)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .frame(width: fieldSize)
+                }
+                
+                VStack(spacing: 20) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("ログイン")
+                            .foregroundColor(.white)
+                            .frame(width: loginBtnWidth, height: loginBtnHeight)
+                            .background(Color(.orange))
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink (
+                        
+                        destination: CreateAccountView()) {
+                        Text("新規登録")
+                            .foregroundColor(.orange)
+                    }
+                }
+                Spacer()
+                    .frame(height: 150)
+                
+                
+            }
+        }
     }
 }
 
