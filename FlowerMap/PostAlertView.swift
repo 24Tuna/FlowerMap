fdfd82b62ca63f7b1d08b4d3cbc38ae3b119dff5
24Tuna <21cm0124@jec.ac.jp>
2021-10-29 12:02:51 +0900

    修正

---
+++ FlowerMap/PostAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  PostAlertView.swift
+//  FlowerMap
+//
+//  Created by cmStudent on 2021/10/29.
+//
+
+import SwiftUI
+
+struct PostAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostAlertView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostAlertView()
+    }
+}
